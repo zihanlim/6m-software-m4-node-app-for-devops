c38a8cd97fd14019b001c839ada164ee0199bf02
zihanlim <zihan0804@gmail.com>
2023-10-09 21:25:20 +0800

    config.yml

--- controller.js
+++ controller.js
@@ -1,7 +1,6 @@
-
 // This is the only function that will be called by the endpoint.
-function print(req, res){
-    res.send("Hello world!");
+function print(req, res) {
+  res.send("Hello world!");
 }
 
 module.exports = print;
--- controller.test.js
+++ controller.test.js
@@ -1,19 +1,18 @@
 const targetFn = require("./controller");
 
-describe("the print function", ()=>{
+describe("the print function", () => {
+  // standard best practice
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
 
-    // standard best practice
-    beforeEach(()=>{
-        jest.clearAllMocks();
-    })
+  it("should respond with 'Hello world!'", () => {
+    const mockReq = {};
+    const mockRes = {
+      send: jest.fn(),
+    };
 
-    it("should respond with 'Hello world!'", ()=>{
-        const mockReq = {};
-        const mockRes = {
-            send:jest.fn()
-        }
-
-        targetFn(mockReq, mockRes);
-        expect(mockRes.send).toHaveBeenCalledWith("Hello world!!"); // If you change this value, the test will fail.
-    })
-})
+    targetFn(mockReq, mockRes);
+    expect(mockRes.send).toHaveBeenCalledWith("Hello world!"); // If you change this value, the test will fail.
+  });
+});
